08f094a555c4ffeb703c5bfae633fdaba908078e
Crew Spence <csspence123@gmail.com>
2020-07-12 06:35:46 -0500

    Wrote working solution.

--- is_palindrome_udemy.js
+++ is_palindrome_udemy.js
@@ -5,8 +5,19 @@ isPalindrome
 Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward).  Otherwise it returns false.
 */
 
-function isPalindrome(){
+const isPalindrome = (str) => {
+  let revStr = '';
+  let ind = str.length - 1;
+  const innerFunc = (string, index) => {
+    if(index < 0) {
+      return '';
+    } else {
+      return string[index] + innerFunc(string, index - 1);
+    }
+  }
+  revStr = innerFunc(str, ind);
 
+  return str === revStr ? true : false;
 }
 
 // isPalindrome('awesome') // false
