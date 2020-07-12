01f391724f89f2c8cbc6d0d8907f6969051ebabc
Crew Spence <csspence123@gmail.com>
2020-07-12 06:23:27 -0500

    Created function stub for toy problem.

---
+++ is_palindrome_udemy.js
@@ -0,0 +1,16 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+isPalindrome
+Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward).  Otherwise it returns false.
+*/
+
+function isPalindrome(){
+
+}
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
